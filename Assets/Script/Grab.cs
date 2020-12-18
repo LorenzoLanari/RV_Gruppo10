59faa8dffabddd59a16fa5a01b00f64fc859becb
Stefano Cataldi <62599281+princata@users.noreply.github.com>
2020-12-18 12:33:40 +0100

    grab scripts

--- Assets/Script/Grab.cs
+++ Assets/Script/Grab.cs
@@ -6,8 +6,12 @@ public class Grab : MonoBehaviour
 {
     public bool grabbing = false;
     public bool mutex = true;
-
+    public bool objectfound = false;
+    private bool patchv2 = false;
+    public Transform GrabPoint;
     private TPC_Rob _tpc;
+    private Pickable boxer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,24 +22,61 @@ public class Grab : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && mutex)
-        {
-            if (grabbing)
-            {
-                mutex = false;
-                grabbing = false;
-                Invoke("Carry", 5.2f);
-            }
-            else
-            {
-                mutex = false;
-                grabbing = true;
-                Invoke("Carry", 5.2f);
-                            
-            }
+        if (objectfound)
+        {     
+            patchv2 = CheckPosition();
         }
+
+        if (patchv2)
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.E) && mutex)
+            {
+             
+                    if (grabbing)
+                    {
+
+                        boxer.transform.SetParent(null);
+                        mutex = false;
+                        grabbing = false;
+                        Invoke("Carry", 5.2f);
+                    }
+                    else
+                    {
+                        boxer.transform.SetParent(GrabPoint);
+                        Physics.IgnoreCollision(boxer.GetComponent<Collider>(), GetComponent<Collider>());
+                        mutex = false;
+                        grabbing = true;
+                        Invoke("Carry", 5.2f);
+                            
+                    }            
+            }
+
+        }
+
     }
     public void Carry() {
         mutex = true;
+    }
+
+    public bool CheckPosition()
+    {
+        Ray raygrab = new Ray(transform.position, transform.forward);
+        RaycastHit raycastHit = new RaycastHit();
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
+        if (Physics.Raycast(raygrab, out raycastHit, 500))
+        {
+            if (raycastHit.collider.GetComponent<Pickable>())
+            {
+                boxer = raycastHit.collider.GetComponent<Pickable>();
+                
+                return true;
+            }
+            else
+                boxer = null;
+        }
+        //Quaternion.AngleAxis(12.0f, transform.forward) * transform.right
+        return false;
     }
 }
---
+++ Assets/Script/Pickable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    public Transform Destination;
+    private Collider mycollider;
+    private Grab rob;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mycollider = GetComponent<Collider>();
+        rob = GameObject.Find("Rob").GetComponent<Grab>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Collider[] hitColliders = Physics.OverlapSphere(Destination.position, 3f);
+        foreach(var hitCollider in hitColliders)
+        {
+            if (hitCollider == mycollider)
+                Debug.Log("arrived to destination");
+        }
+
+
+        if (Vector3.Distance(rob.transform.position, transform.position) < 1.5f)
+        {
+          
+            rob.objectfound = true;     
+        }
+        else
+            rob.objectfound = false;
+
+    }
+}
